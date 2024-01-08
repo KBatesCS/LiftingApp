2e8e11074cf58f671fb298a7d8d45254402fb620
Kevin Bates <KOBates23@gmail.com>
2024-01-08 14:22:31 -0500

    Initial Commit

---
+++ LiftingApp/LiftingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftingAppApp.swift
+//  LiftingApp
+//
+//  Created by Kevin Bates on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LiftingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

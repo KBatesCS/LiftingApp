b5ac4ef15def2ca2a56573cac55892b8189a0287
KBatesCS <54908821+KBatesCS@users.noreply.github.com>
2024-03-21 12:51:19 -0400

    loads from defaultdata.json
    -mae Bundle-Decodable.swift class that loads the exercise list from defaultData.json now instead of the predefined small list.

---
+++ LiftingApp/Screens/VideoAnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoAnalysisView.swift
+//  LiftingApp
+//
+//  Created by Kevin Bates on 3/21/24.
+//
+
+import SwiftUI
+
+struct VideoAnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoAnalysisView()
+}

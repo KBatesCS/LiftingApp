61328b670008f48ef91f575f33d5f87d89559d4f
KBatesCS <54908821+KBatesCS@users.noreply.github.com>
2024-05-13 17:07:14 -0400

    Added Rest Timer
    -after completing a set, a rest time will start counting down
    -issue is that rest time is not actually stores in the workout set display so that needs to be fixed

--- LiftingApp/Screens/ActiveWorkoutView.swift
+++ LiftingApp/Screens/ActiveWorkoutView.swift
@@ -23,12 +23,23 @@ struct ActiveWorkoutView: View {
     @Environment(\.dismiss) private var dismissAction: DismissAction
     
     @State private var elapsedTime = TimeInterval(0)
-    @State private var timer: Timer?
+    @State private var timer: AnyCancellable?
     
     @State private var isShowingFinishConf: Bool = false
     
     var elapsedTimeString: String {
         let currentTimeInterval = elapsedTime
+        let hours = Int(currentTimeInterval / 3600)
+        let minutes = Int((currentTimeInterval.truncatingRemainder(dividingBy: 3600)) / 60)
+        let seconds = Int(currentTimeInterval.truncatingRemainder(dividingBy: 60))
+        
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+    
+    @State private var endRestTime: Date?
+    @State private var remainingRestTime = TimeInterval(0)
+    var remainingRestString: String {
+        let currentTimeInterval = remainingRestTime
         let hours = Int(currentTimeInterval / 3600)
         let minutes = Int((currentTimeInterval.truncatingRemainder(dividingBy: 3600)) / 60)
         let seconds = Int(currentTimeInterval.truncatingRemainder(dividingBy: 60))
@@ -56,24 +67,6 @@ struct ActiveWorkoutView: View {
     
     
     var body: some View {
-        HStack {
-            Spacer()
-            Text(workoutDisplay.workoutName)
-                .scaledToFill()
-                .minimumScaleFactor(0.5)
-                .lineLimit(1)
-            Spacer()
-            
-            Text("\(elapsedTimeString)")
-            
-            Spacer()
-            
-        }
-        
-        Spacer()
-        
-        
-        
         List {
             
             if !workoutDisplay.previousNotes.isEmpty {
@@ -145,6 +138,8 @@ struct ActiveWorkoutView: View {
                                 .onTapGesture {
                                     self.workoutDisplay.exercises[woIndex].sets[sindex].completed.toggle()
                                     self.workoutDisplay.objectWillChange.send()
+                                    self.endRestTime = Calendar.current.date(byAdding: .second, value: 90, to: Date())!
+                                    self.remainingRestTime = 90
                                 }
                             
                         }
@@ -207,55 +202,110 @@ struct ActiveWorkoutView: View {
         .scrollDismissesKeyboard(.interactively)
         
         .overlay {
-            HStack {
-                Spacer()
-                Text("\(elapsedTimeString)")
-                    .frame(alignment: .bottom)
-                    .font(.title2)
-                    .foregroundColor(Color("Text"))
-                    .bold()
-                    .frame(width: UIScreen.main.bounds.width/3.4, height: 42)
-                    .background(Color("Accent"))
-                    .cornerRadius(7)
-                    .padding(.bottom, 6)
-                
-                Button (action: {
-                    //stopTimer()
-                    if !self.allExercisesComplete(displayInfo: workoutDisplay) {
-                        isShowingFinishConf = true
-                    } else {
-                        self.closeActiveWorkoutInfo()
-                        self.saveToRecord(displayInfo: workoutDisplay)
-                        self.dismissAction.callAsFunction()
-                    }
-                }, label: {
-                    Text("End Workout")
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("\(elapsedTimeString)")
                         .frame(alignment: .bottom)
                         .font(.title2)
                         .foregroundColor(Color("Text"))
                         .bold()
-                        .frame(width: UIScreen.main.bounds.width/1.8, height: 42)
+                        .frame(width: UIScreen.main.bounds.width/3.4, height: 42)
                         .background(Color("Accent"))
                         .cornerRadius(7)
                         .padding(.bottom, 6)
-                })
-                .alert("Finish Workout", isPresented: $isShowingFinishConf, actions: {
-                    Button("Finish Anyways", action: {
-                        self.saveToRecord(displayInfo: workoutDisplay)
-                        self.closeActiveWorkoutInfo()
-                        self.dismissAction.callAsFunction()
+                    
+                    Button (action: {
+                        //stopTimer()
+                        if !self.allExercisesComplete(displayInfo: workoutDisplay) {
+                            isShowingFinishConf = true
+                        } else {
+                            self.closeActiveWorkoutInfo()
+                            self.saveToRecord(displayInfo: workoutDisplay)
+                            self.dismissAction.callAsFunction()
+                        }
+                    }, label: {
+                        Text("End Workout")
+                            .frame(alignment: .bottom)
+                            .font(.title2)
+                            .foregroundColor(Color("Text"))
+                            .bold()
+                            .frame(width: UIScreen.main.bounds.width/1.8, height: 42)
+                            .background(Color("Accent"))
+                            .cornerRadius(7)
+                            .padding(.bottom, 6)
                     })
-                    Button("End Without Saving", role: .destructive, action: {
-                        self.closeActiveWorkoutInfo()
-                        self.dismissAction.callAsFunction()
-                    })
-                    Button("Cancel", role: .cancel, action: {})
-                }, message: {Text("Not all exercises have been completed")})
-                Spacer()
+                    .alert("Finish Workout", isPresented: $isShowingFinishConf, actions: {
+                        Button("Finish Anyways", action: {
+                            self.saveToRecord(displayInfo: workoutDisplay)
+                            self.closeActiveWorkoutInfo()
+                            self.dismissAction.callAsFunction()
+                        })
+                        Button("End Without Saving", role: .destructive, action: {
+                            self.closeActiveWorkoutInfo()
+                            self.dismissAction.callAsFunction()
+                        })
+                        Button("Cancel", role: .cancel, action: {})
+                    }, message: {Text("Not all exercises have been completed")})
+                    Spacer()
+                }
+                
+                if let endRestTime = self.endRestTime {
+                    ZStack {
+                        Color(.accent)
+                            .frame(height: 50)
+                        
+                        HStack {
+                            Button(action: {
+                                self.endRestTime = nil
+                            }, label: {
+                                Text("End")
+                                    .bold()
+                                    .font(.system(size: 25)) // Adjust size as needed
+                                    .padding(10) // Add padding around the button content
+                                    .foregroundColor(Color(.text))
+                            })
+                            
+                            Spacer()
+                            
+                            Text("\(self.remainingRestString)")
+                                .bold()
+                                .frame(maxWidth: .infinity)
+                                .font(.system(size: 25))
+                            Spacer()
+                            Button(action: {
+                                self.endRestTime =  endRestTime.addingTimeInterval(-10)
+                                self.remainingRestTime -= 10
+                            }) {
+                                Image(systemName: "minus.square.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 25, height: 25) // Adjust size as needed
+                                    .padding(10) // Add padding around the button content
+                                    .foregroundColor(Color(.text))
+                            }
+                            Spacer()
+                            Button(action: {
+                                self.endRestTime = endRestTime.addingTimeInterval(10)
+                                self.remainingRestTime += 10
+                            }, label: {
+                                Image(systemName: "plus.app.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 25, height: 25) // Adjust size as needed
+                                    .padding(5) // Add padding around the button content
+                                    .foregroundStyle(Color(.text))
+                            })
+                            Spacer()
+                        }
+                    }
+//                    .transition(.move(edge: .bottom))
+//                    .animation(.default)
+                }
             }
             .frame(maxHeight: .infinity, alignment: .bottom)
         }
-        
+        .navigationTitle("\(workoutDisplay.workoutName)")
         .onAppear {
             if self.activeWorkoutInfo.display == nil {
                 if let oldRecord = getLatestRecord() {
@@ -267,15 +317,29 @@ struct ActiveWorkoutView: View {
             }
             self.elapsedTime = Date().timeIntervalSince(workoutDisplay.startTime)
             
-            self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                self.elapsedTime = Date().timeIntervalSince(workoutDisplay.startTime)
-                if (self.elapsedTime >= 864999) { // should stop the timer right before 10 days hits.
-                    self.timer?.invalidate()
+            self.timer = Timer.publish(every: 1, on: .main, in: .common)
+                .autoconnect()
+                .sink { _ in
+                    let elapsedTime = Date().timeIntervalSince(workoutDisplay.startTime)
+                    if elapsedTime >= 864999 {
+                        self.timer?.cancel()
+                    } else {
+                        self.elapsedTime = elapsedTime
+                    }
+                    
+                    if let restEndTime = endRestTime {
+                        let remainingRestTime = restEndTime.timeIntervalSinceNow
+                        if remainingRestTime.isLess(than: 0) {
+                            self.endRestTime = nil
+                        } else {
+                            self.remainingRestTime = remainingRestTime
+                        }
+                    }
+                    
                 }
-            }
         }
         .onDisappear {
-            self.timer?.invalidate()
+            self.timer?.cancel()
         }
     }
     
@@ -321,7 +385,9 @@ struct ActiveWorkoutView: View {
             }
         }
         
+        
     }
+    
     
     func getLatestRecord() -> CDWorkoutRecord? {
         let request = CDWorkoutRecord.fetch()
